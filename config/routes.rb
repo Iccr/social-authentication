9463c435855925ee6f9f0cfae9e7eafbacb69be0
ccr <sis.ccr@gmail.com>
2015-12-29 19:22:00 +0545

    sessions controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'sessions/create'
 
-  get 'home/profile'
+  get 'sessions/destroy'
+
+  root 'home#index'
+
+  get 'home#profile'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
